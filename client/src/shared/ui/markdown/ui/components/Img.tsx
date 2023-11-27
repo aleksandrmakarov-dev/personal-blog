ef6f2d0ecbc9320dcdf41b81a59c3625119c0111
ac5295 <ac5295@student.jamk.fi>
2023-11-27 21:22:05 +0200

    added feature to open image markdown component in new window

--- client/src/shared/ui/markdown/ui/components/Img.tsx
+++ client/src/shared/ui/markdown/ui/components/Img.tsx
@@ -1,16 +1,18 @@
 export const Img = (props: any) => {
   const { node, ...other } = props;
   return (
-    <span className="block">
-      <img
-        className="rounded-md mb-2 max-h-[28rem] max-w-full mx-auto"
-        {...other}
-      />
-      {other.title && (
-        <span className="text-center text-foreground-secondary block">
-          {other.title}
-        </span>
-      )}
-    </span>
+    <a target="_blank" href={props.src}>
+      <span className="block">
+        <img
+          className="rounded-md mb-2 max-h-[28rem] max-w-full mx-auto"
+          {...other}
+        />
+        {other.title && (
+          <span className="text-center text-foreground-secondary block">
+            {other.title}
+          </span>
+        )}
+      </span>
+    </a>
   );
 };
